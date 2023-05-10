53fd06e4d5613a773065635d732de44d2c1f158a
Mikayla Mayumi <58568794+mikaylamrogers@users.noreply.github.com>
2023-05-09 23:08:35 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
 $db_host = "us-cdbr-east-06.cleardb.net";
-$db_user = "b196e6230007c4";
-$db_pass = "dfa15e3d";
-$db_name = "heroku_8e7c8263c2b34f5";
+$db_user = "b7eb69c7de4c8b";
+$db_pass = "ed3447d4";
+$db_name = "heroku_2ebf4257da5c43d";
 
 $connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");
